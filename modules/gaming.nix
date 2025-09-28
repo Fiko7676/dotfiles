1256d4e6b10947c7d1540272ba0d9dedc7d0d2de
Fiko7676 <767fiko@gmail.com>
2025-09-28 12:01:03 +0200

    feat: add gaming things

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,9 +1,36 @@
 {pkgs, ...}: {
-  programs.gamemode.enable = true;
-  programs.gamescope.enable = true;
-  programs.steam.enable = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      intel-ocl
+      intel-vaapi-driver
+      mesa
+      vulkan-loader
+      vulkan-tools
+      vulkan-validation-layers
+    ];
+  };
 
-  environment.systemPackages = with pkgs; [
-    heroic
+  programs.gamescope.enable = true;
+
+  security.wrappers.gamescope = {
+    source = "${pkgs.gamescope}/bin/gamescope";
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_nice+p";
+  };
+
+  programs.gamemode.enable = true;
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+  };
+
+  environment.systemPackages = [
+    pkgs.heroic-unwrapped
+    pkgs.intel-media-driver
+    pkgs.wine
+    pkgs.zlib
   ];
 }
