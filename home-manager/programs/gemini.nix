b1cfdb407bc0ad4109b15eb6651d7c35ca6bb481
Fiko7676 <767fiko@gmail.com>
2025-10-01 15:52:01 +0200

    feat: configured gemini cli

--- home-manager/programs/gemini.nix
+++ home-manager/programs/gemini.nix
@@ -1,5 +1,16 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    gemini-cli
-  ];
+  programs.gemini-cli = {
+    enable = true;
+    settings = {
+      selectedAuthType = "oauth-personal";
+      vimMode = true;
+      preferredEditor = "nvim";
+      mcpServers = {
+        nixos = {
+          command = "nix";
+          args = ["run" "github:utensils/mcp-nixos" "--"];
+        };
+      };
+    };
+  };
 }
