81cdcb3b6a31b3a20fc08ffbf3214b4bc01a9b3b
Fiko7676 <767fiko@gmail.com>
2025-10-04 08:48:34 +0200

    feat: add home-manager backups

--- home-manager/programs/gemini.nix
+++ home-manager/programs/gemini.nix
@@ -13,7 +13,6 @@
           command = "uvx";
           args = ["mcp-obsidian"];
           env = {
-            OBSIDIAN_API_KEY = "a7aa4e90a8b0a5bb3d408f615d8569df2c5c53d1f8211042a3830079505e14be";
           };
         };
       };
