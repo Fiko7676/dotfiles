caad7d1d221587eae18cce0997d7374d6b311871
Fiko7676 <767fiko@gmail.com>
2025-10-27 19:00:19 +0000

    feat: changed locales

--- modules/locales.nix
+++ modules/locales.nix
@@ -11,7 +11,7 @@
     LC_NUMERIC = "de_DE.UTF-8";
     LC_PAPER = "de_DE.UTF-8";
     LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
   services.xserver.xkb = {
